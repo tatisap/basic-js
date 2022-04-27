6979733d47caa290adc9a971431d7a14738b8f27
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-04-27 14:44:48 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let sampleActivityNum = parseInt(sampleActivity);
+  if(typeof sampleActivity !== 'string' || 
+    sampleActivityNum >= 15 || 
+    sampleActivityNum <= 0 || 
+    isNaN(sampleActivityNum)) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity)/(Math.log(2) / HALF_LIFE_PERIOD));
 }
+
+console.log(dateSample('3.142'));
 
 module.exports = {
   dateSample
